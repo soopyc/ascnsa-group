32ca1b8af1dde1f6f3864bec3d468e40399ff0c0
Sophie Cheung <me@soopy.moe>
2025-12-02 17:56:24 +0800

    feat: more informational layout template

--- library_app/__init__.py
+++ library_app/__init__.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import Flask, g
 
 from . import routes
 
 app = Flask(__name__)
 routes.register_routes(app)
+
+
+@app.before_request
+def proc_start_time():
+	g.req_received = datetime.now()
+
+
+@app.context_processor
+def inject_processing_time():
+	return dict(
+		processing_ms=round((datetime.now() - g.req_received).microseconds / 1000, 3)
+	)
