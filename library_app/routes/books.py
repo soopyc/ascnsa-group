71800f46413b0738f6cc58a3b102217e0648fe94
Tmak0244 <tmak0244@gmail.com>
2025-12-03 19:29:19 +0800

    Changed books.py and index.html
    For books.py to work, I changed the compose.yml to include 3306 port so the website can communicate with the database

--- library_app/routes/books.py
+++ library_app/routes/books.py
@@ -1,8 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+import mysql.connector
 
 blueprint = Blueprint("books", __name__)
 
+DB_CONFIG = {
+	"host": "127.0.0.1", 
+    "user": "user",
+    "password": "userresu",
+    "database": "library_app",
+	"port": 3306,
+}
 
 @blueprint.route("/books")
 def handle():
-	raise NotImplementedError()
+    conn = mysql.connector.connect(**DB_CONFIG)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM Books")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    html = "<h1>Books</h1><table border=1>"
+    html += "<p><a href='/books/create'><button>Create New Book</button></a></p>"
+    for row in rows:
+        html += "<tr>" + "".join(f"<td>{v}</td>" for v in row.values()) + "</tr>"
+    html += "</table>"
+    
+    return html
+
+@blueprint.route("/books/create")
+def handle2():
+    raise NotImplementedError()
