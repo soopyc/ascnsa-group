859079cc4c54311e480fe6f4130a15f179e407d9
Sophie Cheung <me@soopy.moe>
2025-12-05 13:59:09 +0800

    chore: add formatter

--- flake.nix
+++ flake.nix
@@ -1,16 +1,40 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+  inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = {nixpkgs, ...}: {
-    devShells.x86_64-linux.default = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  outputs =
+    { treefmt-nix, nixpkgs, ... }:
+    let
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ] (
+          system: f nixpkgs.legacyPackages.${system}
+        );
+      treefmtEval = forEachSystem (
+        pkgs:
+        treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = ".git/config";
+          programs = {
+            ruff-format.enable = true;
+            nixfmt.enable = true;
+            djlint.enable = true;
+          };
+        }
+      );
     in
-      pkgs.mkShellNoCC {
-        packages = with pkgs; [
-          pdm
-          ruff
-          python312
-        ];
-      };
-  };
+    {
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            pdm
+            ruff
+            python312
+          ];
+        };
+      });
+
+      formatter = forEachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+
+    };
 }
